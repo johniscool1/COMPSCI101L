7bb534d784aacff8b060ee4d8837f93465d862c8
johniscool1 <90807293+johniscool1@users.noreply.github.com>
2021-09-29 16:27:46 -0500

    Update assigment2.py

--- Assignment 3/code/assigment2.py
+++ Assignment 3/code/assigment2.py
@@ -1,6 +1,7 @@
-
-
-
+#CS 101 Lab
+#Program 2
+#John Chirpich
+#jwcgnq@umkc.edu
 three = 0
 count = 1
 guess = []
@@ -14,11 +15,14 @@ while count <= 100:
 print("Welcome to the Flarsheim Guesser!")
 check = 1
 while check == 1:
+    list1 = []
+    list2 = []
+    list3 = [] 
     play_again = 0
     count = 0
     print("\nPlease think of a number between and including 1 and 100.\n")
     three = int(input("What is the remainder when your number is divided by 3 ?"))
-    while three > 3:
+    while three >= 3 or three < 0:
         print("The value entered must be less than 3 and more than 0.")
         three = int(input("What is the remainder when your number is divided by 3 ?"))
     while count < len(guess)-1:
@@ -27,7 +31,7 @@ while check == 1:
         count += 1
 
     five = int(input("What is the remainder when your number is divided by 5 ?"))
-    while five > 5:
+    while five >= 5 or five < 0:
         print("The value entered must be less than 5 and more than 0.")
         five = int(input("What is the remainder when your number is divided by 5 ?"))
     count = 0
@@ -37,7 +41,7 @@ while check == 1:
         count += 1
 
     eight = int(input("What is the remainder when your number is divided by 7 ?"))
-    while eight > 7:
+    while eight >= 7 or eight < 0:
         print("The value entered must be less than 7 and more than 0.")
         eight = int(input("What is the remainder when your number is divided by 7 ?"))
     count = 0
@@ -56,7 +60,7 @@ while check == 1:
     elif play_again == "n":
         break
     elif play_again == "y" and play_again == "Y":
-        clear(list1)
-        clear(list2)
-        clear(list3)
+        list1 = []
+        list2 = []
+        list3 = []      
         pass    
